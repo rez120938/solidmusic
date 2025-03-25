ac35a10b3c3b675f8d6b9d7a9bcbb7fb9503e1ab
rez120938 <rezqim683@gmail.com>
2025-03-25 20:01:35 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,8 +17,8 @@ class Configs:
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
     OWNER_ID = int(getenv("OWNER_ID", "0123"))
     SESSION = getenv("SESSION", "session")
-    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/solidprojects")
-    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/solidprojects_chat")
+    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/rezsstoree")
+    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/rezsstoree")
     UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
     AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1800"))
 
