7def248fb9fb895cfbe70ddabfaa6b7bf7a38b08
rez120938 <rezqim683@gmail.com>
2025-03-25 20:02:41 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,7 +19,7 @@ class Configs:
     SESSION = getenv("SESSION", "session")
     CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/rezsstoree")
     GROUP_LINK = getenv("GROUP_LINK", "https://t.me/rezsstoree")
-    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
+    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/rez120938/solidmusic")
     AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1800"))
 
 
